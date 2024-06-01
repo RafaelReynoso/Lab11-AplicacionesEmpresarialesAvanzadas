49e88f1923e799b11326c326ab7274c47001aac7
RafaelReynoso <rafael.reynoso@tecsup.edu.pe>
2024-06-01 12:56:43 -0500

    actualizando

---
+++ WebApplication1/Controllers/DetailsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DetailsController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public DetailsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Details
+        public async Task<IActionResult> Index()
+        {
+              return _context.Details != null ? 
+                          View(await _context.Details.ToListAsync()) :
+                          Problem("Entity set 'StoreContext.Details'  is null.");
+        }
+
+        // GET: Details/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Details == null)
+            {
+                return NotFound();
+            }
+
+            var detail = await _context.Details
+                .FirstOrDefaultAsync(m => m.DetailId == id);
+            if (detail == null)
+            {
+                return NotFound();
+            }
+
+            return View(detail);
+        }
+
+        // GET: Details/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Details/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DetailId,Amount,Price,SubTotal")] Detail detail)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(detail);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(detail);
+        }
+
+        // GET: Details/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Details == null)
+            {
+                return NotFound();
+            }
+
+            var detail = await _context.Details.FindAsync(id);
+            if (detail == null)
+            {
+                return NotFound();
+            }
+            return View(detail);
+        }
+
+        // POST: Details/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DetailId,Amount,Price,SubTotal")] Detail detail)
+        {
+            if (id != detail.DetailId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(detail);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DetailExists(detail.DetailId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(detail);
+        }
+
+        // GET: Details/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Details == null)
+            {
+                return NotFound();
+            }
+
+            var detail = await _context.Details
+                .FirstOrDefaultAsync(m => m.DetailId == id);
+            if (detail == null)
+            {
+                return NotFound();
+            }
+
+            return View(detail);
+        }
+
+        // POST: Details/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Details == null)
+            {
+                return Problem("Entity set 'StoreContext.Details'  is null.");
+            }
+            var detail = await _context.Details.FindAsync(id);
+            if (detail != null)
+            {
+                _context.Details.Remove(detail);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DetailExists(int id)
+        {
+          return (_context.Details?.Any(e => e.DetailId == id)).GetValueOrDefault();
+        }
+    }
+}
